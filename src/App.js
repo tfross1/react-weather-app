4ae338b8a6c0b9cf997fa06ed80aff9dea902c57
tfross1 <theresaefross@gmail.com>
2023-04-23 16:23:00 -0500

    Added GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from "react";
 import './App.css';
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-     Weather App
+     <h1>Weather App</h1>
+     <footer>
+      This project was coded by Theresa Fross and is{" "}
+     <a href="https://github.com/tfross1/react-weather-app" target="_blank" >open-sourced on GitHub.</a>
+     </footer>
     </div>
   );
 }
 
-export default App;
+
