c1329496ace69a7f1a9b7267c5535964098c5f2e
tfross1 <theresaefross@gmail.com>
2023-04-26 16:14:58 -0500

    Added forecast api

---
+++ src/WeatherForecast.js
@@ -0,0 +1,44 @@
+import React, { useState } from "react";
+import WeatherIcon from "./WeatherIcon";
+import "./WeatherForecast.css";
+import axios from "axios";
+
+export default function WeatherForecast(props) {
+	let [loaded, setLoaded] = useState(false);
+	let [forecast, setForecast] = useState(null);
+
+	function handleResponse(response) {
+		setForecast(response.data.daily);
+		setLoaded(true);
+	}
+
+	if (loaded) {
+		return (
+			<div className="WeatherForecast">
+				<div className="row">
+					<div className="col">
+						<div className="WeatherForecast-day">{forecast[0].dt}</div>
+						<WeatherIcon code={forecast[0].weather[0].icon} size={36} />
+						<div className="WeatherForecast-temperatures">
+							<span className="WeatherForecast-temperature-max">
+								{forecast[0].temp.max}°
+							</span>
+							<span className="WeatherForecast-temperature-min">
+								{forecast[0].temp.min}°
+							</span>
+						</div>
+					</div>
+				</div>
+			</div>
+		);
+	} else {
+		let apiKey = "f5029b784306910c19746e40c14d6cd3";
+		let longitude = props.coordinates.lon;
+		let latitude = props.coordinates.lat;
+		let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&long=${longitude}&appid=${apiKey}&units=metric`;
+
+		axios.get(apiUrl).then(handleResponse);
+
+		return null;
+	}
+}
